7cb1a61de0c4f72348c795c21f40ed6a7ca60bc9
Adetola Bewaji <adetola013@gmail.com>
2019-05-11 08:44:26 -0700

    About page added

---
+++ pages/about.js
@@ -0,0 +1,17 @@
+const About = () => (
+ <section>
+     <h2>About Urban Farmers</h2>
+     <p>
+
+         <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sed viverra ipsum nunc aliquet bibendum enim facilisis gravida neque. Nulla aliquet enim tortor at auctor urna nunc id cursus. Convallis posuere morbi leo urna molestie at elementum eu. Non arcu risus quis varius quam quisque id diam. Nisl vel pretium lectus quam id leo in vitae. Neque viverra justo nec ultrices dui. Orci ac auctor augue mauris. Vel facilisis volutpat est velit egestas dui id ornare. Integer eget aliquet nibh praesent</span>
+         
+         <span>Dis parturient montes nascetur ridiculus mus mauris. Elementum facilisis leo vel fringilla est ullamcorper. Faucibus turpis in eu mi bibendum. Risus pretium quam vulputate dignissim suspendisse in. Felis eget nunc lobortis mattis aliquam faucibus purus in. Nibh venenatis cras sed felis eget velit. Scelerisque varius morbi enim nunc. Facilisis mauris sit amet massa vitae tortor condimentum lacinia quis. Elit duis tristique sollicitudin nibh sit amet commodo. Fusce ut placerat orci nulla pellentesque dignissim enim sit amet. Bibendum arcu vitae elementum curabitur vitae nunc sed. Vel elit scelerisque mauris pellentesque. Nulla facilisi morbi tempus iaculis urna id volutpat lacus laoreet. At volutpat diam ut venenatis. Viverra tellus in hac habitasse platea. Ultrices mi tempus imperdiet nulla. Mattis aliquam faucibus purus in massa tempor. Pellentesque habitant morbi tristique senectus et netus et malesuada. At urna condimentum mattis pellentesque.lorem</span>
+         
+         <span>Orci dapibus ultrices in iaculis nunc sed augue lacus. Diam quam nulla porttitor massa id neque aliquam vestibulum. Mattis enim ut tellus elementum sagittis. Vivamus at augue eget arcu dictum varius duis at consectetur. Nunc lobortis mattis aliquam faucibus purus in massa tempor. Nec tincidunt praesent semper feugiat nibh sed pulvinar. Faucibus interdum posuere lorem ipsum dolor sit. Nascetur ridiculus mus mauris vitae. Pulvinar neque laoreet suspendisse interdum consectetur libero id faucibus. Adipiscing vitae proin sagittis nisl. Arcu dui vivamus arcu felis bibendum. Cursus risus at ultrices mi tempus. Elit at imperdiet dui accumsan. Purus semper eget duis at tellus at urna. Integer eget aliquet nibh praesent. Scelerisque in dictum non consectetur a erat nam at</span>
+         
+     </p>
+    
+ </section>
+);
+
+export default About;
